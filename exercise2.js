ca7005600a1d9f100826f270077debb3a8b0e4bf
simo M <simo2@simos>
2023-08-15 12:54:44 -0700

    new

--- exercise2.js
+++ exercise2.js
@@ -2,13 +2,20 @@
 
 //  Using this array, add 3 more elements to the end, and access (read) the 2nd element in the array.
 // [“bob”, “jerry”, “brandon”]
-let myArray = ["bob", "jerry", "brandon"];
-const addNames = (myArray, itemToAdd) => {
-    myArray.forEach(element) => {
-        if (element === itemToAdd) {
-            myArray.push(index, 1)
-        }
-    };
-   
-} 
-console.log(myArray)
+// let myArray = ["bob", "jerry", "brandon"];
+// const addNames = (myArray, itemToAdd) => {
+//     myArray.forEach(element) => {
+//         if (element === itemToAdd) {
+//             myArray.push(index, 1)
+//         }
+//     };
+
+// } 
+// console.log(myArray)
+const classArray = ["Sally", "Sam", "Frodo", "Gandalf", "Eragorn", "Gimley"]
+const addStudentToClass = (student) => {
+    classArray.push(student);
+    console.log(classArray);
+}
+addStudentToClass("Simo");
+addStudentToClass("Learner");
